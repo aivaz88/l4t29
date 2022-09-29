39eaeb48ae222761347b25bbbbbe71d61e4dc4b3
aivaz88 <aivaz1988@mail.ru>
2022-09-29 23:31:55 +0500

    Print Array

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+print_array();
+
+void print_array()
+{
+    int[] new_array = array();
+    for (int i = 0; i < new_array.Length; i++)
+        Console.Write($"{new_array[i]} ");
+}
+
+int[] array()
+{
+    int[] new_array = new int[8];
+    Random randGenerator = new Random();
+    for (int i = 0; i < 8; i++)
+    {
+        new_array[i] = randGenerator.Next();
+    }
+    return(new_array);
+}
